822a60eeca7a00ba006bfc01febc720b7034ffa5
windrain77 <hojinm@gmail.com>
2016-01-13 16:15:06 +0900

    add setSize()

--- src/ch_09/Constructor/Duck.java
+++ src/ch_09/Constructor/Duck.java
@@ -2,8 +2,13 @@ package ch_09.Constructor;
 
 public class Duck {
 
+	int size;
+	
 	public Duck(){
 		System.out.println("Quack");
 	}
 	
+	public void setSize(int newSize){
+		size = newSize;
+	}
 }
