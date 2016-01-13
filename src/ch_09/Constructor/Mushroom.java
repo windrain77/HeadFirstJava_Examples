5daf5577a969c1975f9cd8a46e29914ef8d447a2
windrain77 <hojinm@gmail.com>
2016-01-13 19:48:38 +0900

    add mushroom class, constructor overloading

---
+++ src/ch_09/Constructor/Mushroom.java
@@ -0,0 +1,11 @@
+package ch_09.Constructor;
+
+public class Mushroom {
+	
+	public Mushroom(){}
+	public Mushroom(int size){}
+	public Mushroom(boolean isMagic){}
+	public Mushroom(boolean isMagic, int size){}
+	public Mushroom(int size, boolean isMagic){}
+	
+}
