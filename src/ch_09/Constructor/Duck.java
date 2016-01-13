a5273e85f732d7070a7f0c3a5f815dc8cca34e88
windrain77 <hojinm@gmail.com>
2016-01-13 19:42:59 +0900

    add & use default constructor

--- src/ch_09/Constructor/Duck.java
+++ src/ch_09/Constructor/Duck.java
@@ -3,11 +3,15 @@ package ch_09.Constructor;
 public class Duck {
 
 	int size;
+	
+	public Duck() {
+		size = 27;
+		System.out.println("Quack! size: " + size + " (default)");
+	}
 
 	public Duck(int duckSize) {
-		System.out.println("Quack");
 		this.size = duckSize;
-		System.out.println("size: " + size);
+		System.out.println("Quack! size: " + size);
 	}
 
 	public void setSize(int newSize) {
--- src/ch_09/Constructor/UseDuck.java
+++ src/ch_09/Constructor/UseDuck.java
@@ -1,10 +1,13 @@
 package ch_09.Constructor;
 
 public class UseDuck {
+
 	public static void main(String[] args) {
+	
+		Duck d1 = new Duck(42);
 		
-		Duck d = new Duck(42);
-		d.setSize(42);
+		Duck d2 = new Duck();
 		
 	}
+	
 }
